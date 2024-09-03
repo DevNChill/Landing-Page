3a98568a75c8492d83ea4fefb724a3d6d2b98225
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-09-03 15:48:47 +0530

    came back to add js to sign up buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+let signupbutton = document.querySelectorAll(".signupbutton");
+signupbutton.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert(
+      "Thank You for signing up! (Kidding i don't have any product ideas at the moment XD)",
+    );
+  });
+});
